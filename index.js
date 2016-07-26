bb70c63798c88e1662cf76b632986ae11198416b
Ben Hanna <benpaulhanna@gmail.com>
2016-07-26 03:38:45 +0000

    Chained middleware calls.

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ var app = require('koa')(),
     cors = require('kcors'),
     bodyParser = require('koa-bodyparser');
 
-app.use(cors());
-app.use(bodyParser());
-app.use(require('./v1'));
+app
+    .use(cors())
+    .use(bodyParser())
+    .use(require('./v1'));
 
 app.listen(8080, start);
 
