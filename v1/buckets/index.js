2bc4789d3c1aa05420451780a0e85bb8d7fe729a
Ben Hanna <benpaulhanna@gmail.com>
2016-07-28 16:32:33 +0000

    Formatted knex chains to be more readable.

--- v1/buckets/index.js
+++ v1/buckets/index.js
@@ -16,32 +16,57 @@ module.exports = router.routes();
 
 function* create(next) {
     yield next;
-    var ids = yield buckets().first().insert(this.request.body);
-    this.body = yield buckets().first().where('id', ids.pop());
+    
+    var ids = yield buckets()
+        .first()
+        .insert(this.request.body);
+    
+    this.body = yield buckets()
+        .first()
+        .where('id', ids.pop());
+    
     this.status = 201;
 }
 
 function* get(next) {
     yield next;
-    this.body = yield buckets().first().where(this.params);
+    
+    this.body = yield buckets()
+        .first()
+        .where(this.params);
+    
     this.status = 200;
 }
 
 function* query(next) {
     yield next;
-    this.body = yield buckets().select();
+    
+    this.body = yield buckets()
+        .select();
+    
     this.status = 200;
 }
 
 function* remove(next) {
     yield next;
-    yield buckets().delete().where(this.params);
+    
+    yield buckets()
+        .delete()
+        .where(this.params);
+    
     this.status = 204;
 }
 
 function* update(next) {
     yield next;
-    yield buckets().update(this.request.body).where(this.params);
-    this.body = yield buckets().first().where(this.params);
+    
+    yield buckets()
+        .update(this.request.body)
+        .where(this.params);
+    
+    this.body = yield buckets()
+        .first()
+        .where(this.params);
+    
     this.status = 200;
 }
